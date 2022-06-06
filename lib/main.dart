0d6bb579aa90af2d1d23ae5bdc4aaad193b693a0
Sres0 <susanarepo@gmail.com>
2022-06-05 23:16:04 -0500

    + misc.

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,12 @@ class _MyHomePageState extends State<MyHomePage> {
   double _currentSliderValue = 1;
   bool _currentSwitchState = false;
   Color containerColor = Colors.white;
+  int gestureDetectorValue = 0;
+  int inkwellValue = 0;
 
-  String text = 'Sin sensación';
-  Color color = Colors.green;
-  IconData currentIcon = Icons.favorite;
+  String text = 'Terrible';
+  Color color = Colors.red;
+  IconData icon = Icons.delete_forever_outlined;
 
   List<IconData> icons = [
     Icons.delete_forever_outlined,
@@ -44,37 +46,37 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<String> lables = ['Terrible', 'Malo', 'Moderado', 'Bueno', 'Excelente'];
 
-  int pain = 1;
+  int sliderVal = 1;
 
-  void changeTextAndColor(pain) {
-    String sliderText = lables[0];
-    Color sliderColor = Colors.red;
-    IconData icon = icons[0];
-    if (pain == 1) {
-      sliderText = lables[0];
-      icon = icons[0];
-      sliderColor = Colors.red;
-    } else if (pain <= 2) {
-      sliderText = lables[1];
-      icon = icons[1];
-      sliderColor = Colors.deepOrange;
-    } else if (pain <= 3) {
-      sliderText = lables[2];
-      icon = icons[2];
-      sliderColor = Colors.purple;
-    } else if (pain <= 4) {
-      sliderText = lables[3];
-      icon = icons[3];
-      sliderColor = Colors.blue;
-    } else if (pain <= 5) {
-      sliderText = lables[4];
-      icon = icons[4];
-      sliderColor = Colors.green;
+  void changeTextAndColor(sliderVal) {
+    String _sliderText = lables[0];
+    Color _sliderColor = Colors.red;
+    IconData _icon = icons[0];
+    if (sliderVal == 1) {
+      _sliderText = lables[0];
+      _icon = icons[0];
+      _sliderColor = Colors.red;
+    } else if (sliderVal <= 2) {
+      _sliderText = lables[1];
+      _icon = icons[1];
+      _sliderColor = Colors.deepOrange;
+    } else if (sliderVal <= 3) {
+      _sliderText = lables[2];
+      _icon = icons[2];
+      _sliderColor = Colors.purple;
+    } else if (sliderVal <= 4) {
+      _sliderText = lables[3];
+      _icon = icons[3];
+      _sliderColor = Colors.blue;
+    } else if (sliderVal <= 5) {
+      _sliderText = lables[4];
+      _icon = icons[4];
+      _sliderColor = Colors.green;
     }
     setState(() {
-      text = sliderText;
-      color = sliderColor;
-      currentIcon = icon;
+      text = _sliderText;
+      color = _sliderColor;
+      icon = _icon;
     });
   }
 
@@ -87,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SizedBox(
         height: MediaQuery.of(context).size.height * 0.95,
         child: SingleChildScrollView(
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          child: Column(children: [
             //SLIDER
             Container(
               padding: const EdgeInsets.all(10),
@@ -117,25 +119,83 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                       Row(children: [
                         Text(text, style: TextStyle(color: color)),
-                        Icon(currentIcon, color: color)
+                        Icon(icon, color: color)
                       ], mainAxisAlignment: MainAxisAlignment.spaceEvenly),
                     ],
                   ),
                 ],
               ),
             ),
+            const SizedBox(height: 10),
             //SWITCH
             Container(
                 padding: const EdgeInsets.all(10),
-                color: _currentSwitchState ? Colors.white : Colors.black54,
+                color: _currentSwitchState ? Colors.black54 : Colors.white,
                 height: 100,
                 child: Center(
                     child: Switch(
+                  activeColor: Colors.white,
                   value: _currentSwitchState,
                   onChanged: (value) => setState(() {
                     _currentSwitchState = value;
                   }),
-                )))
+                ))),
+            const SizedBox(height: 10),
+            //Gesture detector e InkWell
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              GestureDetector(
+                  onTap: () => setState(() => gestureDetectorValue += 1),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.blue,
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: 80,
+                      child: Center(
+                        child: Text(
+                          'Gesture detector: $gestureDetectorValue',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ))),
+              InkWell(
+                  borderRadius: BorderRadius.circular(30),
+                  onTap: () => setState(() => inkwellValue += 1),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.black54),
+                      padding: const EdgeInsets.all(10),
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: 80,
+                      child: Center(
+                        child: Text(
+                          'Inkwell: $inkwellValue',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ))),
+            ]),
+            const SizedBox(height: 10),
+            const ExpansionTile(
+              collapsedBackgroundColor: Colors.blue,
+              textColor: Colors.black,
+              backgroundColor: Colors.grey,
+              collapsedTextColor: Colors.white,
+              childrenPadding: EdgeInsets.all(10),
+              title: Text('Expansion tile'),
+              children: [
+                Text(
+                    'El expansion tile sirve para mostrar o esconder los Children del Widget en pantalla.'),
+                ExpansionTile(
+                    childrenPadding: EdgeInsets.all(10),
+                    title: Text('Más información'),
+                    children: [
+                      Text(
+                          'Puedes poner tantos Widgets como quieras, incluso más Expansion tiles.')
+                    ])
+              ],
+            )
           ]),
         ),
       ),
